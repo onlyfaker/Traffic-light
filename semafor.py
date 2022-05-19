5876ba38690e0adcec4c9d78027960491dbbbf59
onlyfaker <gemo.gemaljevic@gmail.com>
2022-05-19 16:53:09 -0400

    Update semafor.py

--- semafor.py
+++ semafor.py
@@ -1,30 +1,34 @@
 import time
 
-start_time = time.time()
-seconds = 1
 while True:
-    current_time = time.time()
-    elapsed_time = current_time - start_time
-    
-    if elapsed_time == seconds:
-    	print("red")
-    	seconds=10
-    	continue
-    current_time2 = time.time()
-    elapsed_time2 = current_time2 - start_time
-    
-    if elapsed_time2 == seconds:
-    	print("yellow")
-    	seconds=2
-    	continue
-    current_time3 = time.time()
-    elapsed_time3 = current_time3 - start_time
-    if elapsed_time3==seconds:
-        print('green')
-        seconds=10
-        continue
-    current_time4 = time.time()
-    elapsed_time4 = current_time4 - start_time
-    if elapsed_time4 == seconds:
-        print("red")
-        seconds=1
+    print('green')
+    start_time = time.time()
+
+    seconds = 10
+
+    while True:
+        current_time = time.time()
+        elapsed_time = current_time - start_time
+
+        if elapsed_time >= seconds:
+            print("yellow")
+            break
+    start_time2 = time.time()
+    seconds2 = 3.5
+
+    while True:
+        current_time2 = time.time()
+        elapsed_time2 = current_time2 - start_time2
+
+        if elapsed_time2 >= seconds2:
+            print("red")
+            break
+    start_time2 = time.time()
+    seconds3 = 10
+
+    while True:
+        current_time = time.time()
+        elapsed_time = current_time - start_time2
+
+        if elapsed_time >= seconds:
+            break
