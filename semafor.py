acbb912f3a8f80dbf5aa209aed716ba8c1b3231c
onlyfaker <gemo.gemaljevic@gmail.com>
2022-05-20 17:33:42 -0400

    Update semafor.py

--- semafor.py
+++ semafor.py
@@ -1,4 +1,26 @@
 import time
+import turtle
+
+
+wn = turtle.Screen()
+wn.title('stoplights')
+wn.bgcolor('black')
+
+pen = turtle.Turtle()
+pen.color('green')
+pen.width(3)
+pen.hideturtle()
+pen.penup()
+pen.goto(-30,60)
+pen.pendown()
+pen.fd(60)
+pen.rt(90)
+pen.fd(120)
+pen.rt(90)
+pen.fd(60)
+pen.rt(90)
+pen.fd(120)
+
 
 while True:
     print('green')
